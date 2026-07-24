463891c93b7c6be93d86d85891a521b7c08c6ac2
diodemusic <diodemusicuk@gmail.com>
2026-07-24 21:08:08 +0100

    Blink an LED

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <Arduino.h>
+#define LED 2
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  pinMode(2, OUTPUT);
+  Serial.begin(115200); // start comms
+  Serial.println("running...");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  digitalWrite(LED, HIGH);
+  delay(100);
+  digitalWrite(LED, LOW);
+  delay(200);
 }
