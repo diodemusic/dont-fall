6e7245b569ae55eaf998d1ed3343ef1f0274ef9b
diodemusic <diodemusicuk@gmail.com>
2026-07-24 21:29:58 +0100

    Hand roll timer dumb

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,36 @@
 #include <Arduino.h>
 #define LED 2
 
+unsigned long timeCurrent = 0;
+unsigned long timeLast = 0;
+const unsigned long TIME_INTERVAL = 1000;
+bool ledState = false;
+
 void setup()
 {
   pinMode(2, OUTPUT);
-  Serial.begin(115200); // start comms
+  // Serial.begin(115200); // start comms
   Serial.println("running...");
 }
 
 void loop()
 {
-  digitalWrite(LED, HIGH);
-  delay(100);
-  digitalWrite(LED, LOW);
-  delay(200);
+  timeCurrent = millis();
+
+  if (timeCurrent - timeLast >= TIME_INTERVAL)
+  {
+    timeLast = timeCurrent;
+    ledState = !ledState;
+
+    if (ledState)
+    {
+      digitalWrite(LED, HIGH);
+      // Serial.println("blink high");
+    }
+    else
+    {
+      digitalWrite(LED, LOW);
+      // Serial.println("blink low");
+    }
+  }
 }
