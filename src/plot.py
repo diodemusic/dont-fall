679dc5d5f6a2790b7c5ef2e80a6b662800a4a810
diodemusic <diodemusicuk@gmail.com>
2026-07-26 01:21:31 +0100

    Plot gyro tilt

--- src/plot.py
+++ src/plot.py
@@ -2,8 +2,8 @@
 """Live plot of the MPU6050 CSV stream from the ESP32.
 
 Expects one sample per line:
-    accelX,accelY,accelZ,gyroX,gyroY,gyroZ,angle
-with accel in g, gyro in deg/s, angle in degrees.
+    accelX,accelY,accelZ,gyroX,gyroY,gyroZ,accelAngle,gyroAngle
+with accel in g, gyro in deg/s, angles in degrees.
 
 Usage:
     python plot_mpu.py                    # uses DEFAULT_PORT
@@ -21,11 +21,11 @@ from matplotlib.animation import FuncAnimation
 DEFAULT_PORT = "/dev/cu.usbserial-0001"
 BAUD = 115200
 WINDOW = 300  # samples held on screen
-CHANNELS = 7
+CHANNELS = 8
 
 ACCEL_RANGE = 2.0  # g, matches ACCEL_CONFIG default
 GYRO_RANGE = 250.0  # deg/s, matches GYRO_CONFIG default
-ANGLE_RANGE = 180.0  # degrees, the full range atan2 can return
+ANGLE_RANGE = 90.0  # degrees on screen; widen if the gyro drifts past it
 
 port = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_PORT
 ser = serial.Serial(port, BAUD, timeout=1)
@@ -39,7 +39,10 @@ accel_lines = [
     ax_accel.plot([], [], label=n)[0] for n in ("accel X", "accel Y", "accel Z")
 ]
 gyro_lines = [ax_gyro.plot([], [], label=n)[0] for n in ("gyro X", "gyro Y", "gyro Z")]
-angle_lines = [ax_angle.plot([], [], label="accel angle", color="tab:purple")[0]]
+angle_lines = [
+    ax_angle.plot([], [], label="accel angle", color="tab:purple", linewidth=1.0)[0],
+    ax_angle.plot([], [], label="gyro angle", color="tab:orange", linewidth=1.4)[0],
+]
 
 ax_accel.set_ylim(-ACCEL_RANGE, ACCEL_RANGE)
 ax_accel.set_ylabel("g")
@@ -54,7 +57,7 @@ ax_gyro.grid(alpha=0.3)
 ax_angle.set_ylim(-ANGLE_RANGE, ANGLE_RANGE)
 ax_angle.set_ylabel("degrees")
 ax_angle.set_xlabel("samples")
-ax_angle.legend(loc="upper right")
+ax_angle.legend(loc="upper right", ncol=2)
 ax_angle.grid(alpha=0.3)
 
 for ax in (ax_accel, ax_gyro, ax_angle):
